e19d5a7b926c26e68cbef63ce2365c1ffd2e22e3
truongginjs <truongginjs@gmail.com>
2020-08-27 08:54:09 +0700

    allow *

--- src/Server.ts
+++ src/Server.ts
@@ -49,17 +49,17 @@ const options: cors.CorsOptions = {
     ],
     credentials: true,
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: function (origin, callback) {
-        // allow requests with no origin 
-        // (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
+    // origin: function (origin, callback) {
+    //     // allow requests with no origin 
+    //     // (like mobile apps or curl requests)
+    //     if (!origin) return callback(null, true);
+    //     if (allowedOrigins.indexOf(origin) === -1) {
+    //         var msg = 'The CORS policy for this site does not ' +
+    //             'allow access from the specified Origin.';
+    //         return callback(new Error(msg), false);
+    //     }
+    //     return callback(null, true);
+    // },
     preflightContinue: false,
 };
 
