bf21dead7c0b28af6600088ced6481238b43a83e
dtrung98 <trung17dinhle@gmail.com>
2020-10-25 09:20:24 +0700

    add log when exe build

--- build.js
+++ build.js
@@ -9,7 +9,14 @@ try {
     fs.copySync('./src/public', './dist/public');
     fs.copySync('./src/views', './dist/views');
     // Transpile the typescript files
-    childProcess.exec('tsc --build tsconfig.prod.json');
+    childProcess.exec('tsc --build tsconfig.prod.json', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      });
 } catch (err) {
     console.log(err);
 }
