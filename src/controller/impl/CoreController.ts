85e91a40be31797829a30aa368f0aba7e8c6d9ff
truongginjs <truongginjs@gmail.com>
2021-01-29 16:29:38 +0700

    add api info/keywords post new keyword

--- src/controller/impl/CoreController.ts
+++ src/controller/impl/CoreController.ts
@@ -8,6 +8,7 @@ import { GetFullKeywordsData } from "@core/usecase/common/GetFullKeywordsData";
 import { GetKeywordsData } from "@core/usecase/common/GetKeywordsData";
 import { CoreUtil } from "@utils/CoreUtil";
 import { SetLocationData } from "@core/usecase/common/SetLocationsData";
+import { SetKeywordsData } from "@core/usecase/common/SetKeywordsData";
 
 @controller("/")
 export class CoreController implements interfaces.Controller {
@@ -21,9 +22,9 @@ export class CoreController implements interfaces.Controller {
         await CoreUtil.sendJsonResponse(new GetLocationData().invoke, res);
     }
 
-    @httpPost('info/locations')
-    private async addLocations(req: express.Request, res: express.Response, next: express.NextFunction) {
-        await CoreUtil.sendJsonResponse(()=>new SetLocationData().invoke([...req.body.locations]),res);
+    @httpPost('info/keywords')
+    private async addKeywords(req: express.Request, res: express.Response, next: express.NextFunction) {
+        await CoreUtil.sendJsonResponse(()=>new SetKeywordsData().invoke([...req.body.keywords]),res);
     }
 
     @httpGet('info/keywords')
